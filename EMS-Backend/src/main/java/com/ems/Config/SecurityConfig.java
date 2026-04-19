fc8ed9b3d71609ce48fcf7dc1cb75701a0830829
kaushiik katariya <kaushik.katariya@argusoft.in>
2026-04-19 13:12:45 +0530

    changes for cors.

--- EMS-Backend/src/main/java/com/ems/Config/SecurityConfig.java
+++ EMS-Backend/src/main/java/com/ems/Config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ems.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,7 +17,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(c -> c.disable()).authorizeHttpRequests(request -> request.anyRequest().permitAll())
+        http.csrf(c -> c.disable())
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(request -> request.anyRequest().permitAll())
                 .formLogin(form -> form.disable());
         return http.build();
     }
